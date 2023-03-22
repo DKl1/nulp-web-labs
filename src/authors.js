4997eef1eaa9c4284383a7de31182d6695ce7513
DKl1 <klymbag@gmail.com>
2023-03-22 19:04:12 +0200

    add linter, initialize npm, improve js files

--- src/authors.js
+++ src/authors.js
@@ -1,17 +1,24 @@
-function loadAuthors(){
-    fetch('http://127.0.0.1:8000/api/v1/author/')
-        .then(response => response.json())
-        .then(data => {
-             const tableBody = document.querySelector('#authors tbody');
-             data.forEach(author => {
-                 const row = document.createElement('tr');
-                 row.innerHTML = `
-                     <td>${author.name}</td>
-                     <td>${author.surname}</td>
-                     <td>${author.patronymic}</td>
-                 `;
-                 tableBody.appendChild(row);
-
-             });
-        }).catch(err => console.log(err));
+async function loadAuthors() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/author/');
+        const data = await response.json();
+        renderAuthors(data);
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+function renderAuthors(data) {
+    const tableBody = document.querySelector('#authors tbody');
+    data.forEach(author => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+      <td>${author.name}</td>
+      <td>${author.surname}</td>
+      <td>${author.patronymic}</td>
+    `;
+        tableBody.appendChild(row);
+    });
+}
+
+loadAuthors();
--- src/books.js
+++ src/books.js
@@ -1,22 +1,28 @@
-function loadBooks() {
-    fetch('http://127.0.0.1:8000/api/v1/book/')
-        .then(response => response.json())
-        .then(data => {
-            const divBody = document.querySelector('#books-articles div');
-            data.forEach(book => {
-                const row = document.createElement('article');
-                row.innerHTML = `
-                    <img src="${book.image}" alt="${book.name}">
-                    <h3>${book.name}</h3> 
-                `;
-                book.authors.forEach(author => {
-                    const authorElement = document.createElement('p');
-                    authorElement.textContent = 'by '  + author.name;
-                    row.appendChild(authorElement);
-                });
-                divBody.appendChild(row);
-            });
-        })
-        .catch(error => console.error(error));
+async function loadBooks() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/book/');
+        const data = await response.json();
+        renderBooks(data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
+function renderBooks(data) {
+    const divBody = document.querySelector('#books-articles div');
+    data.forEach(book => {
+        const row = document.createElement('article');
+        row.innerHTML = `
+      <img src="${book.image}" alt="${book.name}">
+      <h3>${book.name}</h3> 
+    `;
+        book.authors.forEach(author => {
+            const authorElement = document.createElement('p');
+            authorElement.textContent = 'by ' + author.name;
+            row.appendChild(authorElement);
+        });
+        divBody.appendChild(row);
+    });
+}
+
+loadBooks();
